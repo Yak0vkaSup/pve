9e5d8b7e35d8008b37cc36e671fef9c356c8c022
Yak0vkaSup <113983099+Yak0vkaSup@users.noreply.github.com>
2024-09-18 01:05:46 +0200

    pve

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, 'dist')));
 
+
 // Telegram authentication handler
 app.post('/auth/telegram', (req, res) => {
     const userData = req.body;
