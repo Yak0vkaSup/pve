3e60c03567b07de49911cd4fa181a576e7ac5011
IIIADRIENIII <136937257+IIIADRIENIII@users.noreply.github.com>
2024-09-29 15:27:33 +0200

    Minox fixes in bollingerNode and getDataNode

--- src/components/custom_nodes/GetDataFromDbNode.js
+++ src/components/custom_nodes/GetDataFromDbNode.js
@@ -1,50 +1,29 @@
 // Import LiteGraph
 import { LiteGraph } from 'litegraph.js'
 
-export function GetDataFromDbNode() {
+export function GetDataFromDb() {
   // this.addInput('Data', 'number')
-  this.addOutput ("DFo", "number"),("DFh", "number"),("DFl", "number"),("DFc", "number"),("DFv", "number")
+  this.addOutput ("Open", "dataframe");
+  this.addOutput ("High", "dataframe");
+  this.addOutput ("Low", "dataframe");
+  this.addOutput ("Close", "dataframe");
+  this.addOutput ("Volume", "dataframe");
+
+  const getFormattedDate = () => {
+    const date = new Date();
+    return date.toISOString().split('T')[0].replace(/-/g, '/'); // Convert to YYYY/MM/DD
+  };
+
   this.properties = { 
     precision: 1,
     symbol: "TONUSDT",
-    startDate: "DD/MM/YYYY",
-    endDate: "DD/MM/YYYY",
+    startDate: getFormattedDate(),
+    endDate: getFormattedDate(),
    };
-  // Add a button widget to the node
-  this.addWidget('button', 'Get data', null, () => {
-    // When the button is clicked, trigger an event
-    this.sendGraphToServer()
-  })
-
-  // Define sendGraphToServer as a method of the node
-  this.sendGraphToServer = function () {
-    if (this.graph) {
-      const serializedGraph = this.graph.serialize()
-      console.log('Graph JSON:', JSON.stringify(serializedGraph, null, 2))
-
-      // Send the JSON to the Flask API
-      fetch('https://pve.finance/api/receive-graph', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(serializedGraph)
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log('Response from server:', data)
-        })
-        .catch((error) => {
-          console.error('Error sending graph to server:', error)
-        })
-    } else {
-      console.error('Graph is not defined in the node.')
-    }
-  }
 }
 
 // Set the title for the node
-GetDataFromDbNode.title = 'Get data From DB'
+GetDataFromDb.title = 'Get data From DB'
 
 // Register the node with LiteGraph
-LiteGraph.registerNodeType('custom/data/get', GetDataFromDbNode)
+LiteGraph.registerNodeType('custom/data/get', GetDataFromDb)
